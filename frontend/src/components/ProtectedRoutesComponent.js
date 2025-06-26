0716de32346a891267ec16ddcfd6dd12221311c3
Han <hanrongzhang@cvloo.com>
2025-06-26 02:31:54 -0700

    update chat component and header used bootstrap

--- frontend/src/components/ProtectedRoutesComponent.js
+++ frontend/src/components/ProtectedRoutesComponent.js
@@ -1,16 +1,22 @@
 //这是一个 路由守卫组件（Protected Route），用来控制哪些页面需要登录（即需要认证）才能访问。
 import { Outlet, Navigate } from "react-router-dom"; //而 <Outlet /> 的意思就是：“我留一个位置出来，把下面那些子路由（profile/orders）显示在我这里。
+import UserChatComponent from "./user/UserChatComponent";
 
 const ProtectedRoutesComponent = ({admin}) => {
   let auth = false;
   if (admin) {
     let adminAuth = true;
-    if (adminAuth) auth = true; //如果adminAuth为T 那么auth=true
+    return adminAuth ? <Outlet /> : <Navigate to="/login" />;//渲染 <Outlet />，也就是你原本想访问的页面 or 渲染 <Navigate to="/login" />，自动跳转到登录页
   } else {
     let userAuth = true;
-    if (userAuth) auth = true;
+    return userAuth ? (
+      <>
+        <UserChatComponent /> <Outlet />
+      </>
+    ) : (
+      <Navigate to="/login" />
+    );
   }
-  return auth ? <Outlet /> : <Navigate to="/login" />; //渲染 <Outlet />，也就是你原本想访问的页面 or 渲染 <Navigate to="/login" />，自动跳转到登录页
 };
 
 export default ProtectedRoutesComponent
